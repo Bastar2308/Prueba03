7243f330b4b96991bc58db699ed7158a82504db8
Davi_ <Davi_@DAVID>
2019-10-26 16:40:08 -0500

    holaaa agregue cucaracha

---
+++ src/zoologicogit/cucaracha.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoologicogit;
+
+/**
+ *
+ * @author Davi_
+ */
+public class cucaracha {
+    private int numero;
+    private int cantidad;
+    private String tipo;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+}
